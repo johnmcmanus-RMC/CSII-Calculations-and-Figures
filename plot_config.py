4b1ac68eff9a596b1547221cb33024106eb9aadc
John McManus <johnmcmanus@rmc.edu>
2024-03-03 15:42:22 -0500

    Add files via upload

---
+++ plot_config.py
@@ -0,0 +1,42 @@
+# plot_config.py
+# Common to all figures in paper
+
+# Common color palette for 12 stations
+
+colors = [
+    "#5A0A33",  # mulberry (1-alt)
+    "#005745",  # filmpro pthalo green (2-main)
+    "#D80D7B",  # vivid cerise (4-alt)
+    "#00CBA7",  # dali (6-main)
+    #"#FFD7E1",  # "pig pink (8-alt)"
+    "#FFACC6",  #"pale pink (7-alt)")
+    "#6B069F",  # purple (10-alt)
+    "#009FFA",  # azure (13-main)
+    "#ED0DFD",  # famous (13-alt)
+    "#DE0D2E",  # geranium lake (14-alt)
+    "#FF8735",  # crusta (16-alt)
+    "#00F407",  # green (17-main)
+    "#FFE239"   # banana yellow (18-alt)
+]
+
+label_list = [
+    "Portland", 
+    "Boston", 
+    "Newport", 
+    "Montauk", 
+    "The Battery", 
+    "Sandy H.", 
+    "Atlantic C.", 
+    "Sewells P.", 
+    "Willmington", 
+    "Charleston", 
+    "Fernand. B.", 
+    "Key West"
+]
+
+
+
+
+font_family = 'Arial'
+font_size = 12
+label_size = 10
